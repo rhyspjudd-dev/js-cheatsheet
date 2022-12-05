bc690389b83b37bc6d0e17de3e276b764eba534b
rpj-dev <rhyspjudd.dev@gmail.com>
2022-12-05 15:23:32 +0000

    updated events

--- events/videoEndDetection.js
+++ events/videoEndDetection.js
@@ -2,14 +2,10 @@
 document.querySelector('#myVideo').addEventListener('ended',videoEndDetect,false);
 function videoEndDetect(e) {
     // What you want to do after the event
-
-    // GAHHHHHH
 }
 
 /* 
 The HTML
-
-silly message to remove
 
 <video id="myVideo">
 <source src="path/#" type="video/mp4">
