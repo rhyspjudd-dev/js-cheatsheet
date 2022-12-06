128816652a88643301ac47ceac34bb82c1406f0d
rpj-dev <rhyspjudd.dev@gmail.com>
2022-12-06 12:29:15 +0000

    Mouse events

---
+++ events/mouseEv.js
@@ -0,0 +1,12 @@
+const elem = document.querySelector('.some-element')
+
+function someFunction() {
+    // Do something 
+}
+
+elem.addEventListener('mousedown', someFunction);
+elem.addEventListener('mouseup', someFunction);
+elem.addEventListener('onmouseenter', someFunction);
+elem.addEventListener('onmouseleave', someFunction);
+
+
